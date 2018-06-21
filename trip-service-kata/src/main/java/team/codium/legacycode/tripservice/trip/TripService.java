27416e3b74c1a2654d79ae7371d1684da0f1eb07
Luis Rovirosa <luisrovirosa@gmail.com>
2018-06-21 13:00:32 +0200

    Adds trip service kata

---
+++ trip-service-kata/src/main/java/team/codium/legacycode/tripservice/trip/TripService.java
@@ -0,0 +1,32 @@
+package team.codium.legacycode.tripservice.trip;
+
+import team.codium.legacycode.tripservice.user.User;
+import team.codium.legacycode.tripservice.user.UserSession;
+import team.codium.legacycode.tripservice.exception.UserNotLoggedInException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripService {
+
+	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+		List<Trip> tripList = new ArrayList<Trip>();
+		User loggedUser = UserSession.getInstance().getLoggedUser();
+		boolean isFriend = false;
+		if (loggedUser != null) {
+			for (User friend : user.getFriends()) {
+				if (friend.equals(loggedUser)) {
+					isFriend = true;
+					break;
+				}
+			}
+			if (isFriend) {
+				tripList = TripDAO.findTripsByUser(user);
+			}
+			return tripList;
+		} else {
+			throw new UserNotLoggedInException();
+		}
+	}
+	
+}
