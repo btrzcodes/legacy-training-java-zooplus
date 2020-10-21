0a9989b5054791715708df1c6be779060e764111
Luis Rovirosa Mairlot <luis@codium.team>
2020-10-21 16:21:24 +0200

    prepare UserSession for introduce static setter

--- trip-service-kata/src/main/java/team/codium/legacycode/tripservice/user/UserSession.java
+++ trip-service-kata/src/main/java/team/codium/legacycode/tripservice/user/UserSession.java
@@ -4,9 +4,21 @@ import team.codium.legacycode.tripservice.exception.CollaboratorCallException;
 
 public class UserSession {
 
-	private static final UserSession userSession = new UserSession();
-	
-	private UserSession() {
+	private static final UserSession userSession;
+
+	static {
+		userSession = new UserSession(new User()){
+			public User getLoggedUser() {
+				throw new CollaboratorCallException(
+						"UserSession.getLoggedUser() should not be called in an unit test");
+			}
+		};
+	}
+
+	private final User user;
+
+	public UserSession(User loggedUser) {
+		this.user = loggedUser;
 	}
 	
 	public static UserSession getInstance() {
@@ -14,8 +26,6 @@ public class UserSession {
 	}
 
 	public User getLoggedUser() {
-		throw new CollaboratorCallException(
-				"UserSession.getLoggedUser() should not be called in an unit test");
+		return user;
 	}
-
 }
