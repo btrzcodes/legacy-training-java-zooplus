b517192ab74e2851362233d87005c37c3b8f2dd2
Daniel Malpica <daniel.malapica@zooplus.com>
2020-10-26 12:32:19 +0100

    Second iteration of TennisGame refactor

--- tennis-refactoring-kata/src/main/java/TennisGame.java
+++ tennis-refactoring-kata/src/main/java/TennisGame.java
@@ -12,7 +12,7 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName.equals("player1"))
             player1Score += 1;
         else
             player2Score += 1;
@@ -20,46 +20,58 @@ public class TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
         if (isTied()) {
-            score = getScoreName(player1Score);
-        } else if (player1Score >= 4 || player2Score >= 4) {
-            int minusResult = player1Score - player2Score;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            score = getScoreNameWhenTied(player1Score);
+        } else if (anyPlayerScoreIsBiggerThanThree()) {
+            score = getScoreNameWhenAnyIsOverThree();
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = player1Score;
-                else {
-                    score += "-";
-                    tempScore = player2Score;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
-            }
+            score = getScoreNameWhenBothAreUnderThree();
         }
         return score;
+    }
+
+    private String getScoreNameWhenBothAreUnderThree() {
+        return String.format("%s-%s", getIndividualScoreName(player1Score), getIndividualScoreName(player2Score));
+    }
+
+    private String getIndividualScoreName(int score) {
+        String tempScore = "";
+        switch (score) {
+            case 0:
+                tempScore = "Love";
+                break;
+            case 1:
+                tempScore = "Fifteen";
+                break;
+            case 2:
+                tempScore = "Thirty";
+                break;
+            case 3:
+                tempScore = "Forty";
+                break;
+        }
+        return tempScore;
+    }
+
+    private String getScoreNameWhenAnyIsOverThree() {
+        String score;
+        int diffBetweenPlayers = this.player1Score - this.player2Score;
+        if (diffBetweenPlayers == 1) score = "Advantage player1";
+        else if (diffBetweenPlayers == -1) score = "Advantage player2";
+        else if (diffBetweenPlayers >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    private boolean anyPlayerScoreIsBiggerThanThree() {
+        return this.player1Score >= 4 || this.player2Score >= 4;
     }
 
     private boolean isTied() {
         return this.player1Score == this.player2Score;
     }
 
-    private static String getScoreName(int m_score1) {
+    private static String getScoreNameWhenTied(int m_score1) {
         String score;
         switch (m_score1) {
             case 0:
@@ -74,7 +86,6 @@ public class TennisGame {
             default:
                 score = "Deuce";
                 break;
-
         }
         return score;
     }
