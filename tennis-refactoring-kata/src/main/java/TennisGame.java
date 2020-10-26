6de3a3f4a1deb785f1f08cd06a843a08c153f274
Jaime Lopez <jaime.lopez@es-mad-mac0019.zooplus.local>
2020-10-26 12:32:24 +0100

    Tenis match

--- tennis-refactoring-kata/src/main/java/TennisGame.java
+++ tennis-refactoring-kata/src/main/java/TennisGame.java
@@ -1,8 +1,8 @@
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int matchScorePlayer1 = 0;
+    private int matchScorePlayer2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,63 +13,80 @@ public class TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            matchScorePlayer1 += 1;
         else
-            m_score2 += 1;
+            matchScorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (matchScorePlayer1==matchScorePlayer2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = getMatchScoreIfEven();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (matchScorePlayer1>=4 || matchScorePlayer2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getIfPlayerWon();
         }
         else
         {
-            for (int i=1; i<3; i++)
+            score = getRestScores(score);
+        }
+        return score;
+    }
+
+    private String getRestScores(String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = matchScorePlayer1;
+            else { score +="-"; tempScore = matchScorePlayer2;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score +="Love";
+                    break;
+                case 1:
+                    score +="Fifteen";
+                    break;
+                case 2:
+                    score +="Thirty";
+                    break;
+                case 3:
+                    score +="Forty";
+                    break;
             }
+        }
+        return score;
+    }
+
+    private String getIfPlayerWon() {
+        String score;
+        int minusResult = matchScorePlayer1-matchScorePlayer2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private String getMatchScoreIfEven() {
+        String score;
+        switch (matchScorePlayer1)
+        {
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
         }
         return score;
     }
