849cdde13d18c322a53785ce4c7fb9556726f57b
Beatriz <beatriz.almagro@zooplus.com>
2020-10-29 12:09:25 +0100

    Print Date first round: printer tested!

--- print-date/src/test/java/PrintDateTest.java
+++ print-date/src/test/java/PrintDateTest.java
@@ -1,12 +1,29 @@
 import org.junit.Test;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class PrintDateTest {
 	@Test
-	public void printDate() throws Exception {
-		PrintDate printDate = new PrintDate(new Calendar(), new Printer());
+	public void validate_is_printing() throws Exception {
+		Printer spyPrinter = mock(Printer.class);
+		PrintDate printDate = new PrintDate(new Calendar(), spyPrinter);
 
 		printDate.printCurrentDate();
 
 		// How can we test this function?
+		verify(spyPrinter).printLine(any());
+	}
+
+	@Test
+	public void validate_date() throws Exception {
+		Printer spyPrinter = mock(Printer.class);
+		PrintDate printDate = new PrintDate(new Calendar(), spyPrinter);
+
+		printDate.printCurrentDate();
+
+		// How can we test this function?
+		verify(spyPrinter).printLine(any());
 	}
 }
