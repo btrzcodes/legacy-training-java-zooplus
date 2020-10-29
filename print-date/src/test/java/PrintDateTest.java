45040ee0427c4e674ed7c20130d4d35bd39ab61a
Fran Sanchez <franjs88@gmail.com>
2020-10-29 12:28:06 +0100

    Tested with mockito

--- print-date/src/test/java/PrintDateTest.java
+++ print-date/src/test/java/PrintDateTest.java
@@ -1,8 +1,9 @@
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class PrintDateTest {
 	@Test
@@ -19,11 +20,14 @@ public class PrintDateTest {
 	@Test
 	public void validate_date() throws Exception {
 		Printer spyPrinter = mock(Printer.class);
-		PrintDate printDate = new PrintDate(new Calendar(), spyPrinter);
+		Calendar calendarValidator = mock(Calendar.class);
+		Date date = new Date();
+		PrintDate printDate = new PrintDate(calendarValidator, spyPrinter);
+		when(calendarValidator.today()).thenReturn(date);
 
 		printDate.printCurrentDate();
+		verify(spyPrinter).printLine(date.toString());
 
 		// How can we test this function?
-		verify(spyPrinter).printLine(any());
 	}
 }
