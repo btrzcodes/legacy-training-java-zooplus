3bf803cd64217c5da5580bbc6ce2c6fdf43cb8a1
Sergio Redondo De La Vega <Sergio.RedondoDeLaVega@zooplus.com>
2020-10-29 09:56:50 +0100

    golden master kata

--- trivia-golden-master/src/test/java/TriviaTest.java
+++ trivia-golden-master/src/test/java/TriviaTest.java
@@ -1,11 +1,29 @@
 import static org.junit.Assert.*;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
 
 public class TriviaTest {
 
 	@Test
-	public void xxx() {
-		assertTrue(true);
+	public void xxx() throws IOException {
+		PrintStream o = new PrintStream(new File("current.txt"));
+		PrintStream console = System.out;
+		System.setOut(o);
+		GameRunner.main(null);
+
+		File file1 = new File("original.txt");
+		File file2 = new File("current.txt");
+		boolean isTwoEqual = FileUtils.contentEquals(file1, file2);
+
+		assertTrue(isTwoEqual);
 	}
 }
