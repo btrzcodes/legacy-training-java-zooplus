466eb053cb66117315c6a0363771b8c8236f7433
Beatriz <beatriz.almagro@zooplus.com>
2020-10-29 11:13:35 +0100

    Golden master kata second round: refactoring!

--- trivia-golden-master/src/main/java/Game.java
+++ trivia-golden-master/src/main/java/Game.java
@@ -18,15 +18,11 @@ public class Game {
     public  Game(){
     	for (int i = 0; i < 50; i++) {
 			popQuestions.addLast("Pop Question " + i);
-			scienceQuestions.addLast(("Science Question " + i));
-			sportsQuestions.addLast(("Sports Question " + i));
-			rockQuestions.addLast(createRockQuestion(i));
+			scienceQuestions.addLast("Science Question " + i);
+			sportsQuestions.addLast("Sports Question " + i);
+			rockQuestions.addLast("Rock Question" + i);
     	}
     }
-
-	public String createRockQuestion(int index){
-		return "Rock Question " + index;
-	}
 	
 	public boolean isPlayable() {
 		return (howManyPlayers() >= 2);
@@ -41,7 +37,7 @@ public class Game {
 	    inPenaltyBox[howManyPlayers()] = false;
 	    
 	    System.out.println(playerName + " was added");
-	    System.out.println("They are player number " + players.size());
+	    System.out.println("They are player number " + howManyPlayers());
 		return true;
 	}
 	
@@ -54,35 +50,35 @@ public class Game {
 		System.out.println("They have rolled a " + roll);
 		
 		if (inPenaltyBox[currentPlayer]) {
-			if (roll % 2 != 0) {
+			if (isEven(roll)) {
 				isGettingOutOfPenaltyBox = true;
 				
 				System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
-				places[currentPlayer] = places[currentPlayer] + roll;
-				if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-				
-				System.out.println(players.get(currentPlayer) 
-						+ "'s new location is " 
-						+ places[currentPlayer]);
-				System.out.println("The category is " + currentCategory());
-				askQuestion();
+				showPlayerLocAndCats(roll);
 			} else {
 				System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
 				isGettingOutOfPenaltyBox = false;
 				}
 			
 		} else {
-		
-			places[currentPlayer] = places[currentPlayer] + roll;
-			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-			
-			System.out.println(players.get(currentPlayer) 
-					+ "'s new location is " 
-					+ places[currentPlayer]);
-			System.out.println("The category is " + currentCategory());
-			askQuestion();
+			showPlayerLocAndCats(roll);
 		}
 		
+	}
+
+	private void showPlayerLocAndCats(int roll) {
+		places[currentPlayer] = places[currentPlayer] + roll;
+		if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+
+		System.out.println(players.get(currentPlayer)
+				+ "'s new location is "
+				+ places[currentPlayer]);
+		System.out.println("The category is " + currentCategory());
+		askQuestion();
+	}
+
+	private boolean isEven(int roll) {
+		return roll % 2 != 0;
 	}
 
 	private void askQuestion() {
@@ -98,6 +94,8 @@ public class Game {
 	
 	
 	private String currentCategory() {
+
+		// TODO: switch this!
 		if (places[currentPlayer] == 0) return "Pop";
 		if (places[currentPlayer] == 4) return "Pop";
 		if (places[currentPlayer] == 8) return "Pop";
